46e901a53c5f0add218a4d9a2dd4cc4c140be55a
Elham Dermaku <dermaku12@gmail.com>
2023-05-11 15:46:52 +0200

    g

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./services/store";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
